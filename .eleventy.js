73b0a757915a79804d5eddca6aedd5a6f1d7661b
David Eadie <david@eadie.net>
2023-07-13 16:50:08 +0100

    Add method to cache bust css loading
    The CSS ref will be set at build time so will only be busted on each
    build

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addPassthroughCopy('src/images')
+  eleventyConfig.addPassthroughCopy('src/images');
+
+  eleventyConfig.addFilter("bust", (url) => {
+    const [urlPart, paramPart] = url.split("?");
+    const params = new URLSearchParams(paramPart || "");
+    params.set("v", new Date().toISOString());
+    return `${urlPart}?${params}`;
+  });
 
   return {
     dir: { input: 'src', output: 'dist' }
